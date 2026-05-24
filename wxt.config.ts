bbf621c01edc3d87b8e5965c8f36ebb542c8d93f
kalin <142799819+kalinplus@users.noreply.github.com>
2026-05-24 23:55:50 +0800

    feat: init proj

---
+++ wxt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'wxt';
+
+export default defineConfig({
+  manifest: {
+    name: 'Auto Filler',
+    permissions: ['storage', 'activeTab', 'scripting'],
+    host_permissions: ['<all_urls>'],
+  },
+});
